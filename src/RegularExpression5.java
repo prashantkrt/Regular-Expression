f1e81201cf7405345d71f0746860c20b8a9a56e0
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-08-30 12:53:39 +0530

    Regex Commit

--- src/RegularExpression5.java
+++ src/RegularExpression5.java
@@ -16,7 +16,6 @@ public class RegularExpression5 {
         matcher = pattern3.matcher("agent 0");
         System.out.println(matcher.find()); //false
 
-
         Pattern pattern4 = Pattern.compile("^agent[1-6]{2,}$");
         matcher = pattern4.matcher("agent 11");//false since there is a space
         System.out.println(matcher.find());
@@ -24,13 +23,12 @@ public class RegularExpression5 {
         Pattern pattern5 = Pattern.compile("^[aA]gent [0-7]{2,}$");
         matcher = pattern5.matcher("agent 007");
         System.out.println(matcher.find()); //true
-        matcher=pattern5.matcher("Agent 007");
+        matcher = pattern5.matcher("Agent 007");
         System.out.println(matcher.find()); //true
 
         Pattern pattern6 = Pattern.compile("^(a|A|aa)gent \\d{2,}$");
         matcher = pattern6.matcher("aagent 007");
         System.out.println(matcher.find());//true
-
 
         Pattern pattern7 = Pattern.compile("^\\w* \\d{2,}$");
         matcher = pattern7.matcher("agent 007");
@@ -39,6 +37,64 @@ public class RegularExpression5 {
         Pattern pattern8 = Pattern.compile("^[a|A]gent \\d{2,}$");
         matcher = pattern8.matcher("agent 007");
         System.out.println(matcher.find());//true
+
+        //groups
+        Pattern pattern9 = Pattern.compile("(^agent)(\\.)(\\d{2,})$");
+        matcher = pattern9.matcher("agent.007");
+        if (matcher.find()) {
+            // Group 0: Full Match
+            System.out.println("Full match : Group 0 match: " + matcher.group(0));
+            // Group 1
+            System.out.println("Group 1 match:" + matcher.group(1));
+            // Group 2: The second group (month)
+            System.out.println("Group 2 match:" + matcher.group(2));
+            //Group 3:
+            System.out.println("Group 3 match:" + matcher.group(3));
+        } else {
+            System.out.println("No match found");
+        }
+        // matcher.group() is same as matcher.group(0)
+        if (matcher.group().equals("agent.007")) {
+            System.out.println("Full Match");
+        }
+        if (matcher.group(3).equals("007")) {
+            System.out.println("Found");
+        }
+
+        /*
+         * matcher.group() or matcher.group(0):
+         * Returns the entire substring of the input that matches the regex pattern.
+         * Equivalent to matcher.group(0)
+         *
+         *
+         * */
+
+        String regex = "(\\d{2})-(\\d{2})-(\\d{4})";
+        String input = "Date: 29-08-2024";
+
+        Pattern pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            System.out.println("Full match: " + matcher.group(0));  //  Entire match "29-08-2024"
+            System.out.println("Day: " + matcher.group(1));          // First group "29"
+            System.out.println("Month: " + matcher.group(2));        // Second group "08"
+            System.out.println("Year: " + matcher.group(3));         // Third group "2024"
+        }
+
+        // Won't work like this it will give exception No Match Found
+        // It should first match the regex if then we can go the group(0), group(1)..
+        // and so on
+
+//        regex = "(\\d{2})-(\\d{2})-(\\d{4})";
+//        pattern = Pattern.compile(regex);
+//        input = "Date of Joining: 29-08-2024";
+//        matcher=pattern.matcher(input);
+//
+//        if (matcher.group(1).equals("29")) {
+//            System.out.println("String found !!!!");
+//        }
+
     }
 }
 
